963c41e5aed8d8f4c6c5c8a0ccc743fed3e811c5
Vivek-Muruganantham <vivek.muruganantham@gmail.com>
2017-01-23 10:09:47 +0530

    Bug fix in Project Euler 3

--- Python Project Euler/Python Project Euler/Project Euler 3.py
+++ Python Project Euler/Python Project Euler/Project Euler 3.py
@@ -4,8 +4,8 @@ What is the largest prime factor of the number 600851475143 ?"""
 import math
 import PrimeNumber 
 Number = 600851475143
-# Largest prime factor has to less than square root of Number 600851475143
-value = int(math.sqrt(Number))
+# Largest prime factor has to less than twice the square root of Number 600851475143
+value = int(math.sqrt(Number) * 2)
 # For loop from largest number to 1 in descending order
 for i in range(value,1,-1):
     # Check if number is divisible by i; if true check if number is prime
